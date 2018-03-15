42d0e3a453e099050457e9faeeded1fa694592a5
githubcyclist <mouse869@gmail.com>
2018-03-15 07:53:02 -0400

    Update Agent.rb

--- Agent.rb
+++ Agent.rb
@@ -1,1 +1,18 @@
-
+class Agent
+  
+  # Creates a new instance of Agent.
+  def initialize
+    
+  end
+  
+  # Makes the Agent say something.
+  def say(text = "")
+    # TODO: Implement say
+  end
+  
+  # Makes the Agent play an animation.
+  def animate(name)
+    # TODO: Implement animate
+  end
+  
+end
